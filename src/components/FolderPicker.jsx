cd7b0e9aa5d26c440ae12d19949dad4305e0e4f7
mash444start <mashrafetonmoy841@gmail.com>
2026-06-13 03:35:24 +0600

    Update FolderPicker.jsx

--- src/components/FolderPicker.jsx
+++ src/components/FolderPicker.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
+import HiddenFolderList from "./HiddenFolderList";
+
+import { scanHiddenFolders } from "../utils/fileScanner";
 
 export default function FolderPicker() {
+  const [folders, setFolders] = useState([]);
+
+  async function handleSelectFolder() {
+    try {
+      const rootHandle =
+        await window.showDirectoryPicker();
+
+      const hiddenFolders =
+        await scanHiddenFolders(rootHandle);
+
+      setFolders(hiddenFolders);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="home">
       <div className="home-card">
@@ -7,9 +28,18 @@ export default function FolderPicker() {
           Mash Media Viewer
         </h1>
 
-        <button className="select-btn">
+        <button
+          className="select-btn"
+          onClick={handleSelectFolder}
+        >
           Select Folder
         </button>
+
+        {folders.length > 0 && (
+          <HiddenFolderList
+            folders={folders}
+          />
+        )}
       </div>
     </div>
   );
