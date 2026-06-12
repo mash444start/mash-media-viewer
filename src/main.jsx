7b86396445d4e9f5295eac5fdde5e0fa1bd0d88e
mash444start <mashrafetonmoy841@gmail.com>
2026-06-13 03:28:49 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import App from "./App.jsx";
+
+import "./styles/global.css";
+import "./styles/home.css";
+import "./styles/reels.css";
+import "./styles/viewer.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
