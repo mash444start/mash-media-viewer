609dd5779d8a152deb3ee3db5762034c4bbc2874
mash444start <mashrafetonmoy841@gmail.com>
2026-06-13 03:34:44 +0600

    Update HiddenFolderList.jsx

--- src/components/HiddenFolderList.jsx
+++ src/components/HiddenFolderList.jsx
@@ -1,1 +1,17 @@
 
+export default function HiddenFolderList({
+  folders
+}) {
+  return (
+    <div className="folder-list">
+      {folders.map((folder) => (
+        <button
+          key={folder.name}
+          className="folder-card"
+        >
+          📁 {folder.name}
+        </button>
+      ))}
+    </div>
+  );
+}
